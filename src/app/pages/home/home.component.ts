4e88acd217ba385962411c264e4bbd7a6f7c4baf
rickydomingos97 <ricardodomingos97@gmail.com>
2025-03-15 16:28:07 -0300

    feat: redesign structure page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { LinksComponent } from '../../components/links/links.component';
 import { SocialComponent } from '../../components/social/social.component';
+import { AboutMeComponent } from '../../components/about-me/about-me.component';
+
 @Component({
   selector: 'app-home',
-  imports: [LinksComponent, SocialComponent],
+  imports: [LinksComponent, SocialComponent, AboutMeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
